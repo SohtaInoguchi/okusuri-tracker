ecda832b973154c186c68a492b2549234be6f42e
JohnnyDamon <53366209+JohnnyDamon@users.noreply.github.com>
2022-04-24 19:36:33 +0900

    feat: routing for home, login, register

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <button>Login</button>
+        <button>Sign up</button>
+    </div>
+    )   
+}
---
+++ src/RegisterNewRecord.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from 'axios';
+
+
+export default function RegisterNewRecord() {
+    const medicationNameInput = React.createRef<HTMLInputElement>();
+    const pharmacyNameInput = React.createRef<HTMLInputElement>();
+    const hospitalNameInput = React.createRef<HTMLInputElement>();
+    const prescriptionDateInput = React.createRef<HTMLInputElement>();
+  
+    const sendData = (e:any) => {
+      e.preventDefault();
+      axios.post('/register-medication', {
+        medicationName: medicationNameInput.current?.value,
+        pharmacyName: pharmacyNameInput.current?.value,
+        hospitalName: hospitalNameInput.current?.value,
+        prescriptionDate: prescriptionDateInput.current?.value
+      })
+      .then(res => console.log("response", res))
+      .catch(err => console.error(err));
+  
+      medicationNameInput.current!.value = "";
+    }
+    return (
+        <>
+          <input ref={medicationNameInput} type="text" id="name-of-medication" placeholder="Name of medication"/>
+          <input ref={pharmacyNameInput} type="text" id="name-of-pharmacy" placeholder="Name of pharmacy"/>
+          <input ref={hospitalNameInput} type="text" id="name-of-hospital" placeholder="Name of hospital"/>
+          <input ref={prescriptionDateInput} type="date" id="prescription-date" placeholder="Date of prescription"/>
+          <button onClick={sendData}>Send</button>
+        </>
+    );
+}
